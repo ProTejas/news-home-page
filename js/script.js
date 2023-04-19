24253557ba5829784674435c48dfa17616855c77
TeknoTejas <tejas.salunke@teknopoint.in>
2023-04-19 19:30:51 +0530

    css added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 let menuClick = document.querySelector('.menu');
 menuClick.addEventListener('click', function () {
     let getClass = document.querySelector('.nav-list-div-mob').classList;
-    if (getClass[1] == "d-none") {
+    getClass.toggle('nav-show')
+    if (getClass[1] === 'd-none') {
         getClass.remove('d-none');
-    } else {
-        getClass.add('d-none');
     }
 })
